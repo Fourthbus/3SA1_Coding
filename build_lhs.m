9c152af1f81f87c4290d5ae12b999be38a36b96f
Schön Cho <Fourthbus@users.noreply.github.com>
2019-05-13 16:49:45 +0100

    Update build_lhs.m

--- build_lhs.m
+++ build_lhs.m
@@ -2,33 +2,23 @@ function lhsmat = build_lhs(xs,ys)
     np = length(xs)-1;
     psip = zeros (np,np+1);
     
-    for j = 1:np+1;
-        for i = 1:np+1;
-            if j < np+1;
-                [infa(i,j) infb(i,j)] = panelinf(xs(j),xs(j+1),ys(j),ys(j+1),xs(i),ys(i));
-            else
-            end
-            if j == 1;
-                psip(i,j) = infa(i,j);
-            elseif j == np+1;
-                psip(i,j) = infb(i,j-1);
-            else
-                psip(i,j) = infa(i,j)+infb(i,j-1);
-            end
+    for i = 1:np;
+        [psip(i,j=1) infb(i,j=1)] = panelinf(xs(1),xs(2),ys(1),ys(2),xs(i),ys(i);
+        [~ psip(i,j=np+1)] = panelinf(xs(np),xs(np+1),ys(np),ys(np+1),xs(i),ys(i);
+        for j = 2:np;
+            [infa(i,j) infb(i,j)] = panelinf(xs(j),xs(j+1),ys(j),ys(j+1),xs(i),ys(i);
+            psip(i,j) = infa(i,j) + infb(i,j-1);
         end
     end
     lhsmat = zeros(np+1,np+1);
-    for i = 1:np+1;
+    for i = 2:np;
         for j = 1:np+1;
-            if i == 1;
-                lhsmat(i,j)=0;
-            elseif i == np+1;
-                lhsmat(i,j)=0;
-            else
-                lhsmat(i,j) = psip(i+1,j)-psip(i,j);
-            end
+            lhsmat(i,j) = psip(i+1,j)-psip(i,j);
         end
     end
+    %cutta condition gamma_1 and gamma_np+1=0 so...
+    lhsmat(1,1) = 1;
+    lhsmat(np+1,np+1) =1;
 end
 
         
