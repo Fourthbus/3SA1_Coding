a4dba82a3d4db7c8e0d932dfe9dd0d84a2fcaa86
Schoen Cho <fourthbus@gmail.com>
2019-05-16 11:54:34 +0100

    Update EX1.m

--- Week 2/EX1.m
+++ Week 2/EX1.m
@@ -7,6 +7,13 @@ for i=1:length(x);
     theta(i) = sqrt(.45/ReL*(ue)^-6*ueintbit(0,ue,x(i),ue));
 end
 %plot of theta/L
+hold on
 plot(x,theta);
 xlabel('non dimensional position x/L');
 ylabel('non dimensional displacement theta/L');
+
+%calculate blasius
+thetab = 0.664/ReL^.5 .* (x).^.5;
+%plotting blasius
+plot(x,thetab);
+legend('analytical','Blasius','location','Southeast');
