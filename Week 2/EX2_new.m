e18c1bd14e2988952ac1fe2931c84974c022be30
Schoen Cho <fourthbus@gmail.com>
2019-05-20 20:08:34 +0100

    Ex2&3

--- Week 2/EX2_new.m
+++ Week 2/EX2_new.m
@@ -1,38 +1,31 @@
 clear
 close all
-
-%Re and du/dx
-Re = 1e7;
-duedx = -.2;
-
-%set boundary conditions
+%input conditions
+Re = 1e6;
+duedx = -.5;
+%iteration setting & initial conditions
 n = 101;
 laminar = true;
-attached = true;
 x = linspace(0,1,n);
-
-%initialision matrices storing transitiona and separation locations
+ue0 = 1;
+%initialsing transition and seperation pointer
 int = 0;
 ils = 0;
-
-%generating ue
+%generating ue matrix
 for i = 1:n;
-    ue(i) = duedx*x(i)+1;
+    ue(i) = duedx*x(i)+ue0;
 end
-
-%transition loop
-i = 1;
-while laminar && i < n;
-    i = i+1;
-    theta = sqrt(.45/Re*(ue(i))^-6*ueintbit(0,ue(1),x(i),ue(i)));
-    Rethet = theta*Re*(ue(i));
-    m = -Re*(theta)^2*duedx;
+i=1;    %initialising loop
+while laminar && i < n; %laminar loop
+    i = i+1;    %interpretation
+    theta(i) = sqrt(.45/Re*(ue(i))^-6*ueintbit(0,ue(1),x(i),ue(i)));
+    Rethet = theta(i)*Re*(ue(i));
+    m = -Re*(theta(i))^2*duedx;
     H = thwaites_lookup(m);
-    He = laminar_He(H);
-
-    if log(Rethet) >= 18.4*He - 21.74;
-        %If above condition is true, flow is no longer laminar
-        laminar = false;
-        disp([x(i) Rethet/1000]);
+    He(i) = laminar_He(H);
+    if log(Rethet) >= 18.4*He(i) - 21.74;   %laminar check
+        laminar = false;    %laminar flag & end loop
+        int = i;
+        display([x(i), Rethet/1000]);
     end
 end
--- Week 2/EX3_new.m
+++ Week 2/EX3_new.m
@@ -1,87 +1,39 @@
 clear
 close all
-
-%Re and du/dx
+%input conditions
 Re = 1e6;
 duedx = -.5;
-
-%set boundary conditions
+%iteration setting & initial conditions
 n = 101;
 laminar = true;
 x = linspace(0,1,n);
-
-%initialision matrices storing transitiona and separation locations
+ue0 = 1;
+%initialsing transition and seperation pointer
 int = 0;
 ils = 0;
-
-%generating ue
+%generating ue matrix
 for i = 1:n;
-    ue(i) = duedx*x(i)+1;
+    ue(i) = duedx*x(i)+ue0;
 end
-
-%transition loop
-i = 1;
-
-while its == 0 && i < n;
-    thick0(1) = theta(i);
-    thick0(2) = deltaE(i);
-    i = i+1;
-    [delx thickhist] = ode45(@thickdash,[0,x(i)-x(i-1)],thick0);
-    theta(i) = thickhist(length(delx),1);
-    deltaE(i) = thickhist(length(delx),2);
-    He(i) = deltaE(i)/theta(i);
-    %calculate H value (from thickdash alogorithm)
-    if He(i) >= 1.46;
-        H = (11*He(i)+15) / (48*He(i)-59);
-    else
-        H = 2.803;
-    end
-    %loop for turbulent reattachment
-    if ils ~= 0 && He(i) >= 1.58 && itr == 0;
-        itr = i;
-    end
-    %loop for turbulent seperation
-    if He(i) <= 1.46;
-        its = i;
-    end
-end
-
-
-
-
-while laminar && i < n;
-    i = i+1;
-    theta = sqrt(.45/Re*(ue(i))^-6*ueintbit(0,ue(1),x(i),ue(i)));
-    Rethet = theta*Re*(ue(i));
-    m = -Re*(theta)^2*duedx;
+i=1;    %initialising loop
+while laminar && i < n; %laminar loop
+    i = i+1;    %interpretation
+    theta(i) = sqrt(.45/Re*(ue(i))^-6*ueintbit(0,ue(1),x(i),ue(i)));
+    Rethet = theta(i)*Re*(ue(i));
+    m = -Re*(theta(i))^2*duedx;
     H = thwaites_lookup(m);
-    He = laminar_He(H);
-
-    if log(Rethet) >= 18.4*He - 21.74;
-        %If above condition is true, flow is no longer laminar
-        laminar = false;
+    He(i) = laminar_He(H);
+    if log(Rethet) >= 18.4*He(i) - 21.74;   %laminar check
+        laminar = false;    %laminar flag & end loop
         int = i;
-    end
-end
-
-if int ~= 0;
-    disp(['Natural transition at ' num2str(x(int)) ' with Rethet ' num2str(Rethet)]);
-end
-
-%seperation loop
-while attached && i < n;
-    i = i+1;
-    theta = sqrt(.45/Re*(ue(i))^-6*ueintbit(0,ue(1),x(i),ue(i)));
-    Rethet = theta*Re*(ue(i));
-    m = -Re*(theta)^2*duedx;
-
-    if m >= 0.09;
-        %If above condition is true, flow is no longer laminar
-        attached = false;
+    elseif m >= 0.09;   %seperation check
+        laminar = false;
         ils = i;
     end
 end
 
 if int ~= 0;
-    disp(['Seperation at ' num2str(x(ils)) ' with Rethet ' num2str(Rethet)]);
+    disp(['Natural transition at ' num2str(x(int)) ' with Rethet ' num2str(Rethet)]);
+elseif ils ~= 0;
+    disp(['Laminar seperation at ' num2str(x(ils)) ' with Rethet ' num2str(Rethet)]);
 end
